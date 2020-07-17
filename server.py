6e6650cb8e6d6033adc279b28f3606b840549423
Coena Das <coenadas@gmail.com>
2020-07-17 20:57:06 +0530

    My portfolio

---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+import csv
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+
+def write_to_file(data):
+    with open('database.txt', mode='a',encoding='utf-8') as database:
+        name = data["name"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        file = database.write(f'\n{name},{email},{subject},{message}')
+
+
+def write_to_csv(data):
+    with open('database.csv', newline='',mode='a',encoding='utf-8') as database2:
+        name = data["name"]
+        email = data["email"]
+        subject = data["subject"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='|',quoting=csv.QUOTE_MINIMAL )
+        csv_writer.writerow([name,email,subject,message])
+
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        write_to_file(data)
+        return redirect('thank_you.html')
+    else:
+        return 'Something went wrong!'
